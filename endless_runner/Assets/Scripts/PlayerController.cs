a80b003f4252e1c0f98b1e01abcc1ab99b51e9da
minh152Dlz <99807270+minh152Dlz@users.noreply.github.com>
2024-01-30 15:13:05 +0700

    movement and prefabs

---
+++ endless_runner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 direction;
+    public float forwardSpeed;
+
+    private int desiredLane = 1;
+    public float laneDistance = 4;
+    public float jumpForce;
+    public float Gravity = -20;
+    
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.z = forwardSpeed;
+        direction.y += Gravity *Time.deltaTime;
+        if(controller.isGrounded){
+         if(Input.GetKeyDown(KeyCode.UpArrow)){
+            Jump();}
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow)){
+            desiredLane++;
+            if(desiredLane == 3)
+                desiredLane = 2;
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow)){
+            desiredLane--;
+            if(desiredLane == -1)
+                desiredLane = 0;
+        }
+
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+
+        if(desiredLane == 0){
+            targetPosition += Vector3.left * laneDistance;
+        }else if (desiredLane == 2){
+            targetPosition += Vector3.right * laneDistance;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, 80*Time.fixedDeltaTime);//targetPosition;
+    }
+
+    private void FixedUpdate(){
+        controller.Move(direction * Time.fixedDeltaTime);
+    }
+
+    private void Jump(){
+        direction.y = jumpForce;
+    }
+}
